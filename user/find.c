dad7169d167da115e096ca051f5bf4d17ce48431
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2023-04-27 01:42:31 +0200

    update find.c main function

--- user/find.c
+++ user/find.c
@@ -1,9 +1,21 @@
 #include "kernel/types.h"
 #include "user/user.h"
 #include "kernel/fcntl.h"
+#include "kernel/fs.h"
+#include "kernel/stat.h"
+#include "user/ulib.c"
 
 int main(int argc, char *argv[])
 {
-	printf("find 1 >");
-  exit(0);
+	if(argc < 2) {
+		printf("Not enough arguments.");
+	} else if (argc == 2) {
+		find(".", argv[1];
+		//Pfad uebergeben
+	} else {		
+		for(k = 2; i < argc; k++) {
+			find(argv[1], argv[k]);
+		}	
+	}
+	exit(0);
 }
